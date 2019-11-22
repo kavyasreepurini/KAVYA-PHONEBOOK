bb6ccef33eed727259116adb18a13bf643cdb5a5
kavyasreepurini <56908820+kavyasreepurini@users.noreply.github.com>
2019-11-22 14:12:17 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,21 @@
 ﻿ using System;
-//using contacts;
 using System.Collections.Generic;
-//using console;
+
 
 namespace kavyaphonebook
 {
     class Program
     {
-         consoleHandler handler = new consoleHandler();
-        public void Main(string[] args)
+        
+        public static void Main(string[] args)
         {
-             //PhoneBook phonebook = new PhoneBook();
-             
-             
-           int  Option =  handler.listMenu(int selection);
-             
-             
+           consoleHandler handler = new consoleHandler();
+             PhoneBook phonebook = new PhoneBook();
              SelectionSwitch selectionSwitch = new SelectionSwitch();
-             selectionSwitch.SelectionMethod(Option);
-             
+         
+           int  Option =  handler.listMenu();
+           selectionSwitch.SelectionMethod(Option);
+           //phonebook.addfile();
                      
        }
     }   
