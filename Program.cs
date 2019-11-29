e1496b01ba4e2c14468de2537a53b14348c63b6f
kavyasreepurini <kavyasreepurini@gmail.com>
2019-11-29 15:49:47 +0530

    friday commit

--- Program.cs
+++ Program.cs
@@ -1,21 +1,47 @@
 ﻿ using System;
-//using contacts;
 using System.Collections.Generic;
-//using console;
 
-namespace kavyaphonebook
+
+namespace Kavya.Phonebook
 {
     class Program
     {
-         consoleHandler handler = new consoleHandler();
-        public void Main(string[] args)
+        
+        public static void Main(string[] args)
         {
-             PhoneBook phonebook = new PhoneBook();
-             handler.listMenu(int selection);
-             
-             SelectionSwitch switchSelection = new SelectionSwitch();
-             SelectionSwitch.SelectionMethod(selection);
-             
+           ConsoleHandler handler = new ConsoleHandler();
+           PhoneBook phonebook = new PhoneBook();
+           Contact contact = new Contact();
+           
+         
+           int  option =  handler.SelectMenutItem();
+           ActOnSelectMenu(option);
+           phonebook.PrintContactList();
+           phonebook.AddTofile();
+
+         void ActOnSelectMenu(int option)
+           {
+                switch (option)
+              {
+                 case 1:
+                  Contact contactToAdd = handler.GetContact( contact);
+                  phonebook.AddContact(contactToAdd);
+                  break;
+
+                case 2:
+                 Contact contactToDelete = handler.DeleteContact(phonebook.display());
+                 phonebook.DeleteContact(contactToDelete);
+                 break;
+
+                case 3:
+                 phonebook.EditContact();
+                 break;
+                
+                //case 4: phonebook.searchContact();
+                //break;
+        }
+
+           }
                      
        }
     }   
